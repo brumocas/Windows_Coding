7eb101fa7c8dbcd980ba8052b251aaf23a5eb05c
Brumocas <brucosta2002@gmail.com>
2023-09-16 23:15:52 +0100

    Update

--- Java/Intro_to_java/src/com/braching/For.java
+++ Java/Intro_to_java/src/com/braching/For.java
@@ -2,7 +2,7 @@ package com.braching;
 
 public class For {
     public static void main(String[] args) {
-        String[] friends = {"Bruno", "Ines", "Pedro"};
+        String[] friends = {"Bruno", "Ines", "Pedro", "Quim"};
 
         for (String friend : friends) {
             System.out.println(friend);
